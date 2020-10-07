e3a5c38fd265a8394f81faf39fbbdf702f627c71
charlie763 <cwisoff@gmail.com>
2020-10-07 12:42:10 -0600

    add some resource seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Resource.create(
+  title: "Algebra Introduction - Basic Overview - Online Crash Course Review Video Tutorial Lessons",
+  description: "1 hour video goving over basic algebra as you might learn it in high school",
+  lowerGradeBound: 9,
+  upperGradeBound: 12,
+  url: "https://www.youtube.com/watch?v=grnP3mduZkM",
+  subject: "Math",
+  rating: 3
+
+)
+
+Resource.create(
+  title: "Worksheet on Colonial America",
+  description: "Meet the Middle Colonies, which are the present-day states of New York, New Jersey, Delaware and Pennsylvania. What does your child think life was like for colonists in the Middle Colonies? Social studies students will write a descriptive paragraph to show their understanding of life in the Middle Colonies and life in colonial America in general.",
+  lowerGradeBound: 6,
+  upperGradeBound: 8,
+  url: "https://www.education.com/worksheet/article/middle-colonies/",
+  subject: "History",
+  rating: 2
+
+)
+Resource.create(
+  title: "Instructions for Science Volcano",
+  lowerGradeBound: 3,
+  upperGradeBound: 8,
+  url: "https://www.wikihow.com/Make-a-Volcano",
+  subject: "Science",
+  rating: 5 
+
+)
