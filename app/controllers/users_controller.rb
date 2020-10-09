4206396ecb0926440a77c6d1dd4cf27af36762f6
charlie763 <cwisoff@gmail.com>
2020-10-09 12:23:22 -0600

    refactor user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,14 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: { valid: "true", user: user.as_json}
+      render json: { valid: "true", user: {id: user.id, username: user.username}}
     else
       render json: { valid: "false", errorMessages: user.errors.messages }
     end
+  end
+
+  private
+  def user_params
+    params.permit(:username, :password)
   end
 end
