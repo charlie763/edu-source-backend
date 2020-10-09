c79fe11565447f07e80d44238a487dd8cc66221d
charlie763 <cwisoff@gmail.com>
2020-10-09 11:53:26 -0600

    create user action in controller and route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: { errorMessages: user.errors.messages }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:create]
   resources :resources, only: [:create, :index, :show]
 end
