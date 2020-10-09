7c62b75d3830214c0748cd0552c385fb2aefb24f
charlie763 <cwisoff@gmail.com>
2020-10-09 11:59:57 -0600

    add user validation and secure password to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_171442) do
+ActiveRecord::Schema.define(version: 2020_10_09_145714) do
 
   create_table "resources", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2020_10_07_171442) do
     t.string "url"
     t.string "videoUrl"
     t.integer "rating"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
