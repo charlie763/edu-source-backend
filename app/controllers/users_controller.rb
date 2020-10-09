037912fe9b891b0b03f75b565e24d72fba2fca82
charlie763 <cwisoff@gmail.com>
2020-10-09 12:10:51 -0600

    update user create action to respond with json that says whether user is valid or not

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: user
+      render json: { valid: "true", user: user.as_json}
     else
-      render json: { errorMessages: user.errors.messages }
+      render json: { valid: "false", errorMessages: user.errors.messages }
     end
   end
 end
