bed90ba47bf6dc82d00e40236fa7db565c771563
charlie763 <cwisoff@gmail.com>
2020-10-13 10:09:48 -0600

    add initial login logic to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,11 @@
 class SessionsController < ApplicationController
   def create
+    user = User.find_by(username: params[:username])
+    if user&.authenticate(params[:password])
+      token = issue_token(user)
+      render json: { valid: "true", user: {id: user.id, username: user.username}, token: token}
+    else
+      render json: { valid: "false", errorMessages: user.errors.messages }
+    end
   end
 end
