0f73bfadae7ccdd1d55f1e6d2270d006832bac90
charlie763 <cwisoff@gmail.com>
2020-10-13 17:24:23 -0600

    refactor params for create comment action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+  def create
+    Comment.create(
+      text: comment_params[:text], 
+      user_id: comment_params[:userId], 
+      resource_id: comment_params[:resourceId]
+      )
+    render json: { message: 'comment saved'}
+  end
+
+  def index
+
+  end
+
+  private 
+  def comment_params
+    params.permit(:text, :userId, :resourceId)
+  end
 end
