10f984b92d48b7bbfa0e8cd5f674ce18b906bd59
charlie763 <cwisoff@gmail.com>
2020-10-13 16:09:14 -0600

    generate comment resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_145714) do
+ActiveRecord::Schema.define(version: 2020_10_13_220640) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.integer "resource_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "resources", force: :cascade do |t|
     t.string "title"
