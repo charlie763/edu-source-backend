f566313d324df139b76b49d1ebd65a04054d1f5e
charlie763 <cwisoff@gmail.com>
2020-10-15 11:46:22 -0600

    remove resource from playlist

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -10,15 +10,22 @@ class PlaylistsController < ApplicationController
   end
 
   def update
+    playlist = Playlist.find_by(id: params[:id])
+    resource = Resource.find_by(id: params[:resource_id])
     case params[:update]
     when "add resource"
-      playlist = Playlist.find_by(id: params[:id])
-      resource = Resource.find_by(id: params[:resource_id])
       if playlist && resource && playlist.user_id == current_user.id && !playlist.resources.include?(resource)
         playlist.resources << resource
         render json: { message: "resource added to playlist" }
       else
         render json: { message: "unable to add resource to playlist" }
+      end
+    when "remove resource"
+      if playlist && resource && playlist.user_id == current_user.id && playlist.resources.include?(resource)
+        playlist.resources.destroy(resource)
+        render json: { message: "resource removed from playlist" }
+      else
+        render json: { message: "unable to remove resource from playlist" }
       end
     end
   end
