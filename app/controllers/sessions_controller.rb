2ec0de10b12f0d9ea3bb87a9d3fa50812daaf725
charlie763 <cwisoff@gmail.com>
2020-10-21 17:24:49 -0600

    get rid of puts statement

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
-      puts "got here"
       token = issue_token(user)
       render json: { valid: "true", user: {id: user.id, username: user.username}, token: token}
     else
