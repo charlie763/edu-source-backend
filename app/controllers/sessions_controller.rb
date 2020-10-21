339d47991daafbbb078510d64cdc569a0618b92f
charlie763 <cwisoff@gmail.com>
2020-10-21 17:06:26 -0600

    debug production

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
-    puts "user: #{user}"
     if user&.authenticate(params[:password])
+      puts "got here"
       token = issue_token(user)
       render json: { valid: "true", user: {id: user.id, username: user.username}, token: token}
     else
